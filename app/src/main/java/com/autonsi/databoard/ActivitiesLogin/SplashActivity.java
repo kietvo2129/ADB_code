1aaac79c8c5753dcb0057688dcf244ef8c6df4ab
kietvo2129 <vominhkietx201@gmail.com>
2020-08-13 15:48:31 +0700

    kiet up

--- app/src/main/java/com/autonsi/databoard/ActivitiesLogin/SplashActivity.java
+++ app/src/main/java/com/autonsi/databoard/ActivitiesLogin/SplashActivity.java
@@ -22,10 +22,12 @@ import com.autonsi.databoard.NotificationService;
 import com.quickblox.sample.videochat.java.BuildConfig;
 import com.quickblox.sample.videochat.java.R;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class SplashActivity extends AppCompatActivity {
+    String Url = com.autonsi.databoard.Url.webUrl;
     private static final int SPLASH_DELAY = 4000;
     private SharedPreferences sharedPreferences;
     int versionCode = BuildConfig.VERSION_CODE;
@@ -38,8 +40,10 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
         startService(new Intent(this, NotificationService.class));
         sharedPreferences = getSharedPreferences("datalogin", MODE_PRIVATE);
-
-        new read_infor_app().execute("https://raw.githubusercontent.com/javiersantos/AppUpdater/master/app/update-changelog.json");
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        new read_infor_app().execute(Url+"APIProduct/GetApp_info");
     }
 
     private class read_infor_app extends AsyncTask<String, Void, String> {
@@ -52,11 +56,12 @@ public class SplashActivity extends AppCompatActivity {
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
             try {
-                JSONObject jsonObject = new JSONObject(s);
+                JSONArray jsonArray = new JSONArray(s);
+                JSONObject jsonObject = jsonArray.getJSONObject(0);
                 latestVersion = jsonObject.getString("latestVersion").replace("null","");
                 latestVersionCode = jsonObject.getString("latestVersionCode").replace("null","");
                 url = jsonObject.getString("url").replace("null","");
-                //latestVersion = jsonObject.getString("latestVersion").replace("null","");
+                releaseNotes = jsonObject.getString("releaseNotes").replace("null","");
             } catch (JSONException e) {
                 e.printStackTrace();
                 AlerError.Baoloi("Could not connect to server", SplashActivity.this);
@@ -67,10 +72,8 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     private void checkversionapp(){
+        //Toast.makeText(this, latestVersionCode+"    "+latestVersion, Toast.LENGTH_SHORT).show();
         if (latestVersionCode.equals(versionCode+"")&&latestVersion.equals(versionName)){
-            if (getSupportActionBar() != null) {
-                getSupportActionBar().hide();
-            }
 
             new Handler().postDelayed(new Runnable() {
                 @Override
@@ -97,7 +100,7 @@ public class SplashActivity extends AppCompatActivity {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(SplashActivity.this);
         alertDialog.setCancelable(false);
         alertDialog.setTitle("Warning!!!");
-        alertDialog.setMessage(text);
+        alertDialog.setMessage(text + "\n"+ releaseNotes);
         alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
@@ -111,7 +114,8 @@ public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onResume() {
-        checkversionapp();
+        //checkversionapp();
+        new read_infor_app().execute(Url+"APIProduct/GetApp_info");
         super.onResume();
     }
 }
